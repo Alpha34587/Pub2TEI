fa8ae39a08e41d947f780543b551c29224db909e
Simon Meoni <simonmeoni@aol.com>
2016-07-11 12:38:12 +0200

    add trace function and CLI

--- lib/rb/out_generator.rb
+++ lib/rb/out_generator.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class OutGenerator
   attr_accessor :csv_dtd, :xmllint_log
 
@@ -5,22 +7,21 @@ class OutGenerator
     @csv_dtd = File.new("#{_csv_dtd_uri}/list_dtd.csv",  "w+")
     @csv_dtd << "file, dtd\n"
     @xmllint_log = {}
+    @out_file = ""
   end
 
-  def generate_file(output_arg, name, o_xsltproc, e_xmllint, e_xsltproc, s_xsltproc,dtd)
+  def generate_file(o_xsltproc, e_xmllint, e_xsltproc, s_xsltproc, dtd, output_arg, name)
     if (s_xsltproc.success?)
-      out_file = File.open("#{output_arg}/#{name}","w")
-      out_file << "#{o_xsltproc}"
+      @out_file << "#{o_xsltproc}"
     else
       p "ERROR : parsing problem"
       name.gsub!(".xml",".log")
-      out_file = File.open("#{output_arg}/#{name}","w")
-      out_file << "xmllint error : \n"
-      out_file << "-------------------------------------\n"
-      out_file << "#{e_xmllint}\n"
-      out_file << "xsltproc error :\n"
-      out_file << "-------------------------------------\n"
-      out_file << "#{e_xsltproc}\n"
+      @out_file << "xmllint error : \n"
+      @out_file << "-------------------------------------\n"
+      @out_file << "#{e_xmllint}\n"
+      @out_file << "xsltproc error :\n"
+      @out_file << "-------------------------------------\n"
+      @out_file << "#{e_xsltproc}\n"
       populate_dtd(e_xmllint,File.basename(dtd))
     end
   end
@@ -37,5 +38,24 @@ class OutGenerator
   def create_dtd_entry (file,name,dtd)
     name = File.basename(file).gsub!(".xml",".json")
     @csv_dtd << "#{name},#{dtd}\n"
+  end
+
+  def write_file(_output_arg, _name, *args)
+    if (args.length == 0)
+      file = File.open("#{_output_arg}/#{_name}","w")
+      file << @out_file
+      @out_file = ""
+    else
+      if args[1] != ""
+        file = File.open("#{_output_arg}/#{args[0]}/#{_name}","w")
+        file << args[1]
+      end
+    end
+  end
+
+  def self.create_dir(_path)
+    unless Dir.exists?(_path)
+      FileUtils.mkdir_p(_path)
+    end
   end
 end
--- lib/rb/pub2tei.rb
+++ lib/rb/pub2tei.rb
@@ -4,23 +4,34 @@ require 'open3'
 
 class Pub2tei
 
-  def self.main(_input_arg,_meta_arg,output_arg)
+  def self.main(_input_arg,_meta_arg,output_arg,_trace)
     json_dtd = JSON.parse(File.read("../lib/json/dtd.json"))
-    out_generator = OutGenerator.new(output_arg)
-    Dir[_input_arg + "/" + "*.xml"][0..15].each do |file|
 
+    result_path = "#{output_arg}/result"
+    OutGenerator.create_dir(result_path)
+    out_generator = OutGenerator.new(result_path)
+
+    if _trace
+      OutGenerator.create_dir("#{output_arg}/xmllint_dtd_phase0")
+    end
+
+    Dir[_input_arg + "/" + "*.xml"][0..15].each do |file|
       name = File.basename(file)
+      name_json = File.basename(file).sub(".xml",".json")
+      metadata = JSON.parse(File.read("#{_meta_arg}/#{name_json }"))
       xml = File.read(file)
       doc = Nokogiri::XML(xml)
       dtd = json_dtd[doc.internal_subset.system_id]
 
       puts "processing file : #{file} with dtd : #{dtd}"
       o_xmllint,e_xmllint, s_xmllint = Open3.capture3("xmllint --format --valid --path #{dtd} #{file}")
-
+      if _trace
+        out_generator.write_file(output_arg, name, "xmllint_dtd_phase0", o_xmllint)
+      end
       o_xsltproc,e_xsltproc, s_xsltproc = Open3.capture3("xsltproc --novalid ../lib/xsl/remove_dtd.xsl -", :stdin_data=>o_xmllint)
 
-      out_generator.generate_file(output_arg, name, o_xsltproc, e_xmllint, e_xsltproc, s_xsltproc,dtd)
-
+      out_generator.generate_file(o_xsltproc, e_xmllint, e_xsltproc, s_xsltproc,metadata["corpusName"], result_path, name)
+      out_generator.write_file(result_path, name)
       out_generator.create_dtd_entry(file,name,dtd)
 
     end
